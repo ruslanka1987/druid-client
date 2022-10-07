a3826b88b2fd7659c20cc485a370d49a36cd4be3
ruslanka1987 <russel1987@mail.ru>
2022-10-07 14:35:22 +0700

    fixed last function for strings

--- src/Aggregations/LastAggregator.php
+++ src/Aggregations/LastAggregator.php
@@ -5,6 +5,14 @@ namespace Level23\Druid\Aggregations;
 
 class LastAggregator extends MethodAggregator
 {
+    
+    public function __construct(string $metricName, string $outputName = '', string $type = 'long')
+    {
+        $this->type       = $type;
+        $this->metricName = $metricName;
+        $this->outputName = $outputName ?: $metricName;
+    }
+    
     /**
      * Returns the method for the type aggregation
      *
